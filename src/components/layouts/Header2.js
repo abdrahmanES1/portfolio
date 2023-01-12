39bc38b40366889f77a3b5263daaaec4f0eeb3db
abdrahmanES1 <68553016+abdrahmanES1@users.noreply.github.com>
2023-01-12 23:36:48 +0100

    remove note icon in header section

--- src/components/layouts/Header2.js
+++ src/components/layouts/Header2.js
@@ -95,7 +95,7 @@ function Header2({ light, logoSource, toggleMenu, headerToggler }) {
                 <i className="icon-layers"></i>
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link
                 activeClass="active"
                 to="section-blogs"
@@ -105,7 +105,7 @@ function Header2({ light, logoSource, toggleMenu, headerToggler }) {
               >
                 <i className="icon-note"></i>
               </Link>
-            </li>
+            </li> */}
             <li>
               <Link
                 activeClass="active"
